0b06b603478141991f5e17ca3e7baf4cb828ac4c
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-09-22 16:20:49 -0700

    first attempt to clean up the path to the template files

--- javaScripts/routes/routes.js
+++ javaScripts/routes/routes.js
@@ -2,12 +2,12 @@ app.config(function($routeProvider){
 	$routeProvider.
 		when('/', {
 			// template: '<ul><li ng-repeat="project in projects"><a href="http://localhost:4000/portfolio/#/[[project.name]]">[[project.name]]</a></li></ul>',
-			templateUrl: '../javascripts/templates/projects.html',
+			templateUrl: '../javaScripts/templates/projects.html',
 			controller: 'portfolioCtrl'
 		}).
 		when('/:projectName', {
 			// template: '<h1>Project Name</h1><a href="http://localhost:4000/portfolio/#/">back</a>',
-			templateUrl: '../javascripts/templates/project.html',
+			templateUrl: '../javaScripts/templates/project.html',
 			controller: 'projectDetailCtrl'
 		}).
 		otherwise({
